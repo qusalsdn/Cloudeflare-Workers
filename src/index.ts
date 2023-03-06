da6d80f3c7c016cbe6c0c2172c5f1ed391f4d8c6
변민우 <yja05092@gmail.com>
2023-03-06 15:37:34 +0900

    Routing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 export interface Env {}
 
+// @ts-ignore
+import home from "./home.html";
+
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("get works!!");
+    const url = new URL(request.url);
+    if (url.pathname === "/") {
+      return new Response(home, {
+        headers: {
+          "Content-Type": "text/html;charset=utf-8",
+        },
+      });
+    }
+    return new Response(null, {
+      status: 404,
+    });
   },
 };
